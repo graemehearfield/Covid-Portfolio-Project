b9cc9cf45985d183ad101c9b151932c43a292105
Graeme Hearfield <graemehearfield@hey.com>
2023-06-05 12:35:13 +1200

    Update PortfolioProject1.sql

--- PortfolioProject1.sql
+++ PortfolioProject1.sql
@@ -8,6 +8,7 @@ ORDER by 3,4
 --Order BY 3,4
 
 -- Select data that we are going to be using
+
 SELECT Location, date, total_cases, new_cases, total_deaths, population
 From PortfolioProject..CovidDeaths
 Order by 1,2
@@ -15,9 +16,10 @@ Order by 1,2
 
 -- Look at total cases vs total deaths
 
-SELECT Location, date, total_cases, total_deaths, CAST(total_deaths AS float)/CAST(total_cases AS float)*100 as PercentPopulationInfected
+SELECT Location, date, total_cases, total_deaths, CAST(total_deaths AS float)/CAST(total_cases AS float)*100 as DeathPercentage
 From PortfolioProject..CovidDeaths
 Where Location = 'New Zealand'
+and continent is not NULL
 Order by 1,2
 
 -- Looking at total cases vs population
@@ -58,3 +60,10 @@ Where continent is NULL
 Group by Location
 Order by TotalDeathCount desc 
 
+-- Global Numbers
+
+SELECT date, SUM(new_cases), SUM(new_deaths), SUM(cast(new_deaths as float))/Sum(CAST(new_cases as float))*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
+Where continent is not NULL
+Group by date 
+Order by 1,2
